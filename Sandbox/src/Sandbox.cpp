8bfa4f37e79ca8dd1b0d1dce3f4acdb4cb0724ac
rahul b <f20190134@pilani.bits-pilani.ac.in>
2020-10-22 11:46:51 +0530

    opengl test

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -3,12 +3,50 @@
 
 #include "Gray/Layers/ImguiLayer.h"
 
+
+#include "Platform/Opengl/test/MainTest.h"
+#include "Platform/Opengl/test/TestMenu.h"
+
+class TestLayer : public Gray::ImguiLayer
+{
+public:
+	void OnAttatch() override
+	{
+		ImguiLayer::OnAttatch();
+		testMenu = new Gray::test::TestMenu();
+		testMenu->addTest<Gray::test::MainTest>("Main Test");
+	}
+
+	void OnUpdate() override
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		ImguiBegin();
+
+		testMenu->onImGUIRender();
+		testMenu->onRender();
+
+		ImguiEnd();
+	}
+
+private:
+	Gray::test::TestMenu* testMenu;
+};
+
 class Sandbox : public Gray::Application
 {
 public:
 	Sandbox()
 	{
-		AddLayer(new Gray::ImguiLayer());
+
+		AddLayer(new TestLayer());
+
+		// Init
+		glEnable(GL_BLEND);
+		glEnable(GL_DEPTH_TEST);
+
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		//EndInit
 	}
 
 	~Sandbox()
