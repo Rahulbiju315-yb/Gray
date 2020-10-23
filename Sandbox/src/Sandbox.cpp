89766742932f06893f72dedaa497a478a2dae8ef
rahul b <f20190134@pilani.bits-pilani.ac.in>
2020-10-23 13:57:13 +0530

    test camera

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -4,33 +4,114 @@
 #include "Gray/Layers/ImguiLayer.h"
 
 
-#include "Platform/Opengl/test/MainTest.h"
-#include "Platform/Opengl/test/TestMenu.h"
+#include "Platform/Opengl/test/Util.h"
+#include "Platform/Opengl/Opengl.h"
+#include "glm/glm.hpp"
+#include "GLFW/glfw3.h"
 
-class TestLayer : public Gray::ImguiLayer
+class TestLayer : public Gray::Layer
 {
 public:
 	void OnAttatch() override
 	{
-		ImguiLayer::OnAttatch();
-		testMenu = new Gray::test::TestMenu();
-		testMenu->addTest<Gray::test::MainTest>("Main Test");
+		Gray::Util::sampleObject2(vb, ib1, ib2, va, shader);
+
+		tex = new Gray::Texture("res/textures/t4.jpg", GL_RGB, GL_RGB);
+		tex->bind();
+
+		shader->bind();
+		shader->setUniform("tex1", 0);
+
+		renderer = new Gray::Renderer();
+
+		camera = new Gray::Camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+		mX = -1;
+		mY = -1;
+	}
+
+	void OnDetatch() override
+	{
+		delete shader;
+		delete vb;
+		delete va;
+		delete ib1;
+		delete ib2;
+		delete renderer;
 	}
 
 	void OnUpdate() override
 	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		shader->setUniform("model", glm::mat4(1.0f));
+		shader->setUniform("view", camera->GetView());
 
-		ImguiBegin();
+		shader->setUniform("projection",  camera->GetProjection());
 
-		testMenu->onImGUIRender();
-		testMenu->onRender();
+		renderer->clear();
+		renderer->draw(*va, *ib1, *shader);
+	}
 
-		ImguiEnd();
+	void OnMouseMoved(Gray::MouseMovedEvent& e) override
+	{
+		const glm::vec2& dir = camera->GetDir();
+		
+		float mXn = e.GetX();
+		float mYn = e.GetY();
+
+		if (mX == -1)
+		{
+			mX = mXn;
+			mY = mYn;
+		}
+		else
+		{
+			camera->SetDir(dir.x + (mXn - mX)/20.0f , dir.y);
+		}
+		mX = mXn;
+		mY = mYn;
+	}
+
+	void OnKeyPressed(Gray::KeyPressedEvent& e) override
+	{
+		GRAY_INFO("Key pressed event handled");
+		if (e.GetKeyCode() == Gray::KeyCodes::Key_W)
+		{
+			const glm::vec3& pos = camera->GetPos();
+			camera->SetPos(pos.x, pos.y, pos.z - dr);
+		}
+
+		else if (e.GetKeyCode() == Gray::KeyCodes::Key_A)
+		{
+			const glm::vec3& pos = camera->GetPos();
+			camera->SetPos(pos.x - dr, pos.y, pos.z);
+		}
+
+		else if (e.GetKeyCode() == Gray::KeyCodes::Key_S)
+		{
+			const glm::vec3& pos = camera->GetPos();
+			camera->SetPos(pos.x, pos.y, pos.z + dr);
+		}
+
+		else if (e.GetKeyCode() == Gray::KeyCodes::Key_D)
+		{
+			const glm::vec3& pos = camera->GetPos();
+			camera->SetPos(pos.x + dr, pos.y, pos.z);
+		}
+
 	}
 
 private:
-	Gray::test::TestMenu* testMenu;
+	Gray::Shader* shader;
+	Gray::VertexBuffer* vb;
+	Gray::VertexArray* va;
+	Gray::IndexBuffer* ib1;
+	Gray::IndexBuffer* ib2;
+	Gray::Texture* tex;
+	Gray::Renderer* renderer;
+	Gray::Camera* camera;
+
+	const float dr = 0.1;
+	float mX, mY;
 };
 
 class Sandbox : public Gray::Application
