e5f132a9a94b939beaeb0c2d6e8c35685253482a
rahul b <f20190134@pilani.bits-pilani.ac.in>
2020-10-23 13:40:34 +0530

    added opengl include file

---
+++ Gray/src/Platform/Opengl/Opengl.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Platform/Opengl/Shader.h"
+#include "Platform/Opengl/Texture.h"
+#include "Platform/Opengl/VertexBuffer.h"
+#include "Platform/Opengl/IndexBuffer.h"
+#include "Platform/Opengl/Renderer.h"
+#include "Platform/Opengl/Camera.h"
